aac7829d6b83ee0a540a91cc2926cf083fceaf81
huiwon <hui_0113@naver.com>
2025-05-16 15:53:28 +0900

    404 add

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <h1>Supa Smoothies</h1>
         <Link to="/">Home</Link>
         <Link to="/create">Create New Smoothies</Link>
+        
       </nav>
       <Routes>
         <Route path='/' element={<Home />}/>
